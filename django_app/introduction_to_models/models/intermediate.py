5cc9441c9b7560dc16dcecf2413e73685423b68f
Yunsik Park <space214@gmail.com>
2017-06-07 05:43:38 +0900

    property적용(과제)

--- django_app/introduction_to_models/models/intermediate.py
+++ django_app/introduction_to_models/models/intermediate.py
@@ -7,9 +7,21 @@ class Player(models.Model):
     def __str__(self):
         return self.name
 
-        # current_club 프로퍼티에 현재 속하는 Club리턴
-        # current_tradeinfo 프로퍼티에 현재 자신의 TradeInfo 리턴
+    # current_club 프로퍼티에 현재 속하는 Club리턴
+    @property
+    def current_club(self):
+        return self.club_set.get(
+            tradeinfo__date_leaved__exact=None
+        )
 
+    # current_tradeinfo 프로퍼티에 현재 자신의 TradeInfo 리턴
+    @property
+    def current_tradeinfo(self):
+        name  = TradeInfo.objects.get(
+            player = self.id,
+            date_leaved= None
+        )
+        return '{}, 현 {}소속 {}입단'.format(name.player, name.club, name.date_leaved)
 
 class Club(models.Model):
     name = models.CharField(max_length=40)
@@ -35,10 +47,31 @@ class TradeInfo(models.Model):
     date_joined = models.DateField()
     date_leaved = models.DateField(null=True, blank=True)
     # recommender = models.ForeignKey(Player, on_delete=models.CASCADE)
+    recommender = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
     # prev_club = 이전 Club
+    prev_club = models.ForeignKey(
+        Club,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
 
+
+    def __str__(self):
+        return '{} {} {} {} {}'.format(self.player.id, self.player, self.club, self.date_joined, self.date_leaved)
     # 1. property로 is_current 속성이 TradeInfo가 현재 현직(leaved하지 않았는지)여부 반환
+    @property
+    def is_current(self):
+        return self.date_leaved is None
+
     # 2. recommender와 prev_club을 활성화시키고 Club의 MTM필드에 through_fields를 명시
 
 
     # 위의 요구조건들을 만족하는 실행코드 작성
+
+
