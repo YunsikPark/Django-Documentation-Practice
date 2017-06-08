8466a12d8330e5cd683fb7099252df0c092dbe72
Yunsik Park <space214@gmail.com>
2017-06-08 14:08:51 +0900

    MTM ㅍ필드 ->Intermediate로 전활할때 마이그레이션 관리

--- django_app/introduction_to_models/admin.py
+++ django_app/introduction_to_models/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from .models import Post, Comment, User, Tag
 from .models import Person
 
 admin.site.register(Person)
+# admin.site.register(Post, Comment, User, Tag)
--- django_app/introduction_to_models/models/mixin.py
+++ django_app/introduction_to_models/models/mixin.py
@@ -29,6 +29,19 @@ class Post(TimeStampedMixin):
     author = models.ForeignKey('User', on_delete=models.CASCADE)  # User클래스가 아래에 있기 때문에 문자열 표시를 해 줘야 한다.
     title = models.CharField(max_length=50)
     content = models.TextField()
+    # 이 Post에 좋아요를 누른사람들
+    like_user = models.ManyToManyField(
+        'User',
+        through='PostLike',
+        related_name='like_posts',
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name=None
+    )
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(TimeStampedMixin):
@@ -39,6 +52,22 @@ class Comment(TimeStampedMixin):
 
 class User(TimeStampedMixin):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=100)
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     db_table = 'introduction_to_models_post_like_user'
 
 
 """
@@ -56,5 +85,8 @@ Post모델
     
     def like_post(self, user):
         return '해당 user의 PostLike를 생성 이 후 생성 객체를 리턴'
+    
+    def add_tag(self, tag_name):
+        return '해당 tag_name의 Tag를 생성 또는 기존항목 가져와서 Post에 추가 이후 Tag리턴'
 
 """
