d67381a7d4fb35e43d15e757f311c3897ad7df6f
Yunsik Park <space214@gmail.com>
2017-06-08 17:16:23 +0900

    making queries practice

--- django_app/introduction_to_models/admin.py
+++ django_app/introduction_to_models/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Post, Comment, User, Tag
+from .models import Post, Comment, User, Tag, PostLike
 from .models import Person
 
 admin.site.register(Person)
-# admin.site.register(Post, Comment, User, Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(User)
+admin.site.register(PostLike)
--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'introduction_to_models',
     'django_extensions',
+    'introduction_to_models',
+    'making_queries',
 ]
 
 MIDDLEWARE = [
